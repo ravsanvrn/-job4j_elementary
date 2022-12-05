3a538842081af118cd1d080e595cc6b63557b4f7
Ravsan <ravsan@bk.ru>
2022-12-05 16:43:21 +0300

    6. DSL c AssertJ [#504879 #367827]

--- src/test/java/ru/job4j/MainTest.java
+++ src/test/java/ru/job4j/MainTest.java
@@ -10,4 +10,30 @@ public class MainTest {
         int result = 1;
         assertThat(result).isEqualTo(excepted);
     }
+
+
+    //1. Проверка целочисленных значений.
+    @Test
+    public void whenEquals() {
+        int result = 1;
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    //2. Проверка чисел с плавающей точкой.
+    @Test
+    public void whenFloatPoint() {
+        float result = 1.0000001F;
+        float expected = 1F;
+        assertThat(result).isCloseTo(expected, offset(0.01F));
+    }
+
+    //3. Равенство массивов.
+    @Test
+    public void whenArray() {
+        int[] result = {1, 2, 3};
+        int[] expected = {1, 2, 3};
+        assertThat(result).containsExactly(expected);
+    }
+
 }
